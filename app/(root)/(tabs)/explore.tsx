66b11484b856151ba13ef6b150f1060ea3a953c8
MUhammad Rafiq <rafiq85825@gmail.com>
2024-12-22 21:33:26 +0500

    user auth and profile description is added

---
+++ app/(root)/(tabs)/explore.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Explore = () => {
+return (
+<SafeAreaView  className="h-full bg-white">
+    <View>
+    <Text>Explore</Text>
+    <Text>
+    </Text>
+    </View>
+</SafeAreaView>
+)
+}
+
+export default Explore
